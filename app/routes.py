7d5f6348ed4963e37c44ded9df5d008ec4f75e28
linqinyang <1462738568@qq.com>
2021-12-01 10:07:10 +0800

    Develop microblog on flash Day two

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,20 @@
+from flask import render_template
+
 from . import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World!"
-
+    user = {'username': 'Lim_QgyG'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, title='Lim-QgyG', posts=posts)
