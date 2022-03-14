bfeba35215565ed48093637890491187a91c7d02
linqinyang <1462738568@qq.com>
2022-03-14 16:50:50 +0800

    followed and follower

---
+++ app/init.py
@@ -0,0 +1,19 @@
+import os
+from logging.handlers import RotatingFileHandler
+
+import app
+
+if not app.debug:
+    # ...
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
+                                       backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')
